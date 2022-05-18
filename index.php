996095f1c97c51b7081960578d3f9b891a1b0df6
ronaldo guimaraes <ronaldogmc@hotmail.com>
2022-05-18 20:52:27 -0300

    agr ok

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Nova atualização 456";
-=======
+
   echo "Nova atualização 123";
   
-  echo "Olá Guima";
->>>>>>> bc233e400de6639b1cbab0e8169eabced8ad0122
-
-	echo "Olá Sonia";
-	
 ?>
